1c0b2e7cb43e527cb93bad3d0164ec6a8cd82fe4
ddONGzaru <taewook.jang@epapyrus.com>
2014-03-30 16:54:55 +0900

    팩토리 패턴 예제 추가.

---
+++ zaru_study/src/com/epapyrus/study/designpattern/factory/SoySourceChicken.java
@@ -0,0 +1,9 @@
+package com.epapyrus.study.designpattern.factory;
+
+public class SoySourceChicken extends Chicken {
+
+	public SoySourceChicken() {
+		
+		this.setName("둘이 먹다 셋이 죽어도 모를 간장 치킨");
+	}
+}
